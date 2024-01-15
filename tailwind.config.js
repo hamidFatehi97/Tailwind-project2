e85c6f9d4761e90837c2f2f8175880a98a28b7b0
hamidreza_fatehi <hfhfhfhf485@gmail.com>
2024-01-15 13:22:08 +0330

    second project first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        josefin: ['Josefin Sans'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      }
+    },
+  },
+  plugins: [],
+}
